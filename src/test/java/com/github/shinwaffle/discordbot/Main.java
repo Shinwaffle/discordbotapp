538731d0ecf2ed995ab8859ffc59fbcde834c9fa
Shinwaffle <59802882+Shinwaffle@users.noreply.github.com>
2021-03-22 11:50:54 -0500

    now I can use the token without people seeing it! hopefully ...

--- src/test/java/com/github/shinwaffle/discordbot/Main.java
+++ src/test/java/com/github/shinwaffle/discordbot/Main.java
@@ -14,8 +14,7 @@ import org.javacord.api.DiscordApiBuilder;
 public class Main {
     
     public static void main(String[] args) {
-        String token = "";
-        
+        String token = hastoken.token;
         
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
