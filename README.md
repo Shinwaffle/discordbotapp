558b491a339c5898452a81c68087726cb2f32afd
Shinwaffle <59802882+Shinwaffle@users.noreply.github.com>
2021-03-22 11:40:17 -0500

    initial
    accidentally published my token to the public, yikes. Making a new repo which doesn't include it. Gonna probs use my token and then after I'm done with testing and stuff remove it. idk

---
+++ README.md
@@ -0,0 +1,6 @@
+# My journey to learning java
+
+Hi! I'm someone who's interesting in programming and has started their journey with java. Currently a student in high school and taking a computer science class.
+
+I know the basics of java, I took a class on it at my school. Now I'm trying to learn more advanced topics on it.
+I'm using Visual Studio Basic as my IDE, Maven as my builder, JavaCord as my dependancy to build the bot, and using junit for testing
---
+++ src/test/java/com/github/shinwaffle/discordbot/Main.java
@@ -0,0 +1,32 @@
+package com.github.shinwaffle.discordbot;
+
+/**
+ * if you want testing stuff with junit
+ * 
+ * import static org.junit.Assert.*;    
+ * 
+ * import org.junit.Test;
+ */
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String token = "";
+        
+        
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        //to get invite: api.createBotInvite()
+        System.out.println("Bot is running");
+    }
+}
