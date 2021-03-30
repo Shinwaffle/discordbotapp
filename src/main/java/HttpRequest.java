6a668d068ceb63d582fe0a0ffdfd85dcd3120a5e
Shinwaffle <59802882+Shinwaffle@users.noreply.github.com>
2021-03-30 15:06:01 -0500

    messing with config files and getting everything from the json that I need

--- src/main/java/HttpRequest.java
+++ src/main/java/HttpRequest.java
@@ -3,7 +3,7 @@ import kong.unirest.Unirest;
 public class HttpRequest {
 
  
-  String url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20div&answers=2&site=stackoverflow&filter=!9_bDE(fI5"; 
+  String url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20div&answers=2&site=stackoverflow&filter=!)rTkr_OQen1do4oxw7QI"; 
 
   /**
    * Also, currently I have it set to retrieve the JSON of the first answer.
@@ -16,8 +16,10 @@ public class HttpRequest {
       .getBody()
       .getObject()
       .getJSONArray("items")
+      .getJSONObject(0)
+      .getJSONArray("answers")
       .getJSONObject(index)
-      .toString();
+      .getString("body");
 
       return result;  
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,18 +13,19 @@ public class Main {
         int[] indices = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29}; //there has to be a way better way of doing this lmao
         api.addMessageCreateListener(event -> {
             Message message = event.getMessage();
+            
             System.out.println(message);
             if (message.getContent().equalsIgnoreCase("!ping")) {
                 new MessageBuilder()
                 .append("Here's what I got: ")
-                .appendCode("json", s.getResponse(0)).send(event.getChannel());
+                .appendCode("md", s.getResponse(0)).send(event.getChannel());
             }
             
             for (int i = 0; i < 30; i++) {
                 if (message.getContent().equalsIgnoreCase("!ping "+indices[i])) {
                     new MessageBuilder()
                     .append("Here's what I got: ")
-                    .appendCode("json", s.getResponse(i)).send(event.getChannel());
+                    .appendCode("html", s.getResponse(i)).send(event.getChannel());
                 }
             }
             /**
